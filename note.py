33233f75b78f19329cb0a209d64ba3ddebbe3c98
NOHITESFAYE <nohitesfayemarara@gmail.com>
2026-07-09 15:56:50 +0300

    day2 python class

---
+++ note.py
@@ -0,0 +1,12 @@
+#here is the error of the code you provided
+print("hello world")
+
+# a variable is a a name that holds a value you assign it using =
+sami_age=20
+print(sami_age)
+# use lowecase with underscores for variable names
+#make names descrptive and meaningful
+#cant start with a number
+#avoid using words like if or for
+student_name="sami"
+print(student_name,sami_age)
